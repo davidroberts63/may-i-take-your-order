d421b57172ae687de7bfcc7edf0bdf3f65550e8b
David Roberts <david.roberts63@gmail.com>
2018-06-23 23:40:17 -0500

    Separate classes for types of order items

--- Mityo/OrderItem.cs
+++ Mityo/OrderItem.cs
@@ -1,6 +1,33 @@
 namespace Exam
 {
-    public class OrderItem
+    public interface IOrderItem
     {
+        public Item Item { get; }
+        public int Quantity { get; }
     }
+
+    public class ServiceOrderItem : IOrderItem
+    {
+        public Item Item { get; private set; }
+        public int Quantity { get; private set; }
+
+        public ServiceOrderItem(Item item, int quantity)
+        {
+            this.Item = item;
+            this.Quantity = quantity;
+        }
+    }
+
+    public class MaterialOrderItem : IOrderItem
+    {
+        public Item Item { get; private set; }
+        public int Quantity { get; private set; }
+
+        public MaterialOrderItem(Item item, int quantity)
+        {
+            this.Item = item;
+            this.Quantity = quantity;
+        }
+    }
+
 }

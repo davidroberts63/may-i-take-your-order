5b7611983b32a7cbc4deed55339dfa4232901abb
David Roberts <david.roberts63@gmail.com>
2018-06-24 22:31:23 -0500

    Fix item tests for decimal

--- Tests/ItemTests.cs
+++ Tests/ItemTests.cs
@@ -8,29 +8,29 @@ namespace Tests
         [Fact]
         public void GetKeyShouldReturnKeyGivenInCtor()
         {
-            var item = new Exam.Item(1, "Burger", 2.50F);
+            var item = new Exam.Item(1, "Burger", 2.50m);
             Assert.Equal(1, item.GetKey());
         }
 
         [Fact]
         public void GetNameShouldReturnNameGivenInCtor()
         {
-            var item = new Exam.Item(1, "Burger", 2.50F);
+            var item = new Exam.Item(1, "Burger", 2.50m);
             Assert.Equal("Burger", item.GetName());
         }
 
         [Fact]
         public void GetPriceShouldReturnPriceGivenInCtor()
         {
-            var item = new Exam.Item(1, "Burger", 2.50F);
-            Assert.Equal(2.50F, item.GetPrice());
+            var item = new Exam.Item(1, "Burger", 2.50m);
+            Assert.Equal(2.50m, item.GetPrice());
         }
 
         [Fact]
         public void HashcodeForItemsWithSameDataShouldMatch()
         {
-            var itemA = new Exam.Item(1, "Burger", 2.50F);
-            var itemB = new Exam.Item(1, "Burger", 2.50F);
+            var itemA = new Exam.Item(1, "Burger", 2.50m);
+            var itemB = new Exam.Item(1, "Burger", 2.50m);
 
             Assert.Equal(itemA.GetHashCode(), itemB.GetHashCode());
         }
